3236d83563e4d4b58bfeeb2293a9573638ac43ad
Mateusz <matiutd888@gmail.com>
2023-12-08 21:50:47 +0100

    wip 2

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,6 +1,11 @@
-import qualified Data.Map as M
-type Loc = Integer -- Location relative to rbp
+module Compile where
 
-data Env = {
-    variables :: 
-}
+import qualified Data.Map as M
+import qualified Grammar.AbsLatte as A
+
+type Loc = Integer
+
+data Env = Env
+  { variables :: M.Map A.UIdent Loc, -- Location relative to rbp
+    variableTypes :: M.Map A.UIdent A.Type
+  }
