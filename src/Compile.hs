47eb7e193d7af4eedf5caf631e3411cbe94ff2c4
Mateusz <matiutd888@gmail.com>
2023-12-18 03:40:22 +0100

    Implement declarations

--- src/Compile.hs
+++ src/Compile.hs
@@ -3,6 +3,7 @@ module Compile where
 import qualified Control.Applicative as DM
 import Control.Monad.Except
 import Control.Monad.Identity
+import Control.Monad.Reader
 import Control.Monad.State
 import qualified Data.DList as DList.DList
 import qualified Data.Map as M
@@ -11,6 +12,8 @@ import qualified Data.Text.Lazy as T
 import Data.Text.Lazy.Builder (toLazyText)
 import qualified Grammar.AbsLatte as A
 import Grammar.PrintLatte (printTree)
+import System.Posix.Internals (puts)
+import System.Process (CreateProcess (env))
 import Utils
 import UtilsX86 (codeLines)
 import qualified UtilsX86 as U
@@ -19,6 +22,8 @@ type Loc = Int
 
 type StmtTEval a = StateT Env (ExceptT String IO) a
 
+type ExprTEval a = ReaderT Env (ExceptT String IO) a
+
 data LabelWriter = LWriter
   { lFunName :: String,
     lClassName :: String,
@@ -26,11 +31,11 @@ data LabelWriter = LWriter
   }
 
 data Env = Env
-  { eVarLocs :: M.Map A.UIdent Loc, -- Location relative to rbp
-    eVarTypes :: M.Map A.UIdent A.Type,
-    eCurrClass :: Maybe A.UIdent,
+  { eCurrClass :: Maybe A.UIdent,
     eClasses :: M.Map A.UIdent ClassType,
     eFunctions :: M.Map A.UIdent A.Type,
+    eVarLocs :: M.Map A.UIdent Loc, -- Location relative to ebp
+    eVarTypes :: M.Map A.UIdent A.Type,
     eWriter :: LabelWriter,
     eLocalVarsBytesCounter :: Int
   }
@@ -71,14 +76,33 @@ epilogue =
             ]
     }
 
+runExprTEval :: Env -> ExprTEval a -> IO (Either String a)
+runExprTEval env e = runExceptT (runReaderT e env)
+
+liftExprTEval :: ExprTEval a -> StmtTEval a
+liftExprTEval e = do
+  env <- get
+  liftIO (runExprTEval env e) >>= liftEither
+
+evalExpr :: A.Expr -> ExprTEval U.X86Code
+evalExpr = undefined
+
 getNumberOfBytesForLocals :: A.Stmt -> Int
-getNumberOfBytesForLocals (A.SBStmt _ (A.SBlock _ stmts)) = sum $ map getNumberOfBytesForLocals stmts
-getNumberOfBytesForLocals (A.SDecl _ t items) =
-  let bytes = U.sizeOfTypeBytes t
-   in bytes * length items
-getNumberOfBytesForLocals (A.SCond _ _ s) = getNumberOfBytesForLocals s
-getNumberOfBytesForLocals (A.SCondElse _ _ s1 s2) = maximum $ map getNumberOfBytesForLocals [s1, s2]
-getNumberOfBytesForLocals _ = 0
+getNumberOfBytesForLocals = fst . getNumberOfBytesForLocalsHelper 0 0
+  where
+    getNumberOfBytesForLocalsHelper :: Int -> Int -> A.Stmt -> (Int, Int)
+    getNumberOfBytesForLocalsHelper currMax currDecl (A.SBStmt _ (A.SBlock _ stmts)) =
+      let (resMax, resDeclCtr) = foldl (uncurry getNumberOfBytesForLocalsHelper) (currMax, currDecl) stmts
+       in (resMax, currDecl)
+    getNumberOfBytesForLocalsHelper currMax currDecl (A.SDecl _ t items) =
+      let bytes = U.sizeOfTypeBytes t
+       in let newCurr = currDecl + bytes * length items
+           in (max currMax newCurr, newCurr)
+    getNumberOfBytesForLocalsHelper currMax currDecl (A.SCond _ _ s) = let (newMax, newDecl) = getNumberOfBytesForLocalsHelper currMax currDecl s in (newMax, currDecl)
+    getNumberOfBytesForLocalsHelper currMax currDecl (A.SCondElse _ _ s1 s2) =
+      let res = map (getNumberOfBytesForLocalsHelper currMax currDecl) [s1, s2]
+       in (maximum (map fst res), currDecl)
+    getNumberOfBytesForLocalsHelper x y _ = (x, y)
 
 writeArgumentsToLocationsMap :: [A.Type] -> [A.UIdent] -> M.Map A.UIdent Int -> M.Map A.UIdent Int
 writeArgumentsToLocationsMap types idents currMap = fst $ foldl f (currMap, U.returnAddressOffset) $ zip types idents
@@ -91,6 +115,45 @@ writeArgumentsToLocationsMap types idents currMap = fst $ foldl f (currMap, U.re
 
 writeArgumentsToTypesMap :: [A.Type] -> [A.UIdent] -> M.Map A.UIdent A.Type -> M.Map A.UIdent A.Type
 writeArgumentsToTypesMap types idents currMap = foldl (\m (ident, t) -> M.insert ident t m) currMap $ zip idents types
+
+generateCode :: A.Stmt -> StmtTEval U.X86Code
+generateCode (A.SEmpty _) = return $ mempty
+generateCode (A.SBStmt _ (A.SBlock _ stmts)) = do
+  env <- get
+  code <- mconcat <$> mapM generateCode stmts
+  newEnv <- get
+  put $
+    newEnv
+      { eVarLocs = eVarLocs env,
+        eVarTypes = eVarTypes env
+      }
+  return code
+generateCode (A.SDecl _ t items) = foldM addDeclCode mempty items
+  where
+    addDeclCode :: U.X86Code -> A.Item -> StmtTEval U.X86Code
+    addDeclCode currCode item = do
+      itemCode <- handleItem item
+      return $ itemCode <> currCode
+
+    handleDecl :: A.UIdent -> StmtTEval ()
+    handleDecl ident = do
+      ctr <- gets eLocalVarsBytesCounter
+      let variableOffset = ctr - U.sizeOfTypeBytes t
+      env <- get
+      put
+        env
+          { eLocalVarsBytesCounter = variableOffset,
+            eVarLocs = M.insert ident variableOffset (eVarLocs env),
+            eVarTypes = M.insert ident t (eVarTypes env)
+          }
+      return ()
+
+    handleItem :: A.Item -> StmtTEval U.X86Code
+    handleItem (A.SNoInit _ ident) = handleDecl ident >> return mempty
+    handleItem (A.SInit _ ident expr) = do
+      handleDecl ident
+      liftExprTEval (evalExpr expr)
+generateCode _ = undefined
 
 compileFunction :: A.UIdent -> [A.UIdent] -> A.Block -> StmtTEval U.X86Code
 compileFunction name idents body = do
@@ -106,8 +169,8 @@ compileFunction name idents body = do
             lCounter = 1
           }
 
-  let funLocations = writeArgumentsToLocationsMap argTypes idents DM.empty
-  let funLocalTypes = writeArgumentsToTypesMap argTypes idents DM.empty
+  let funLocations = writeArgumentsToLocationsMap argTypes idents M.empty
+  let funLocalTypes = writeArgumentsToTypesMap argTypes idents M.empty
   let funLocalVarsBytesCounter = 0
   let newEnv =
         env
@@ -116,7 +179,8 @@ compileFunction name idents body = do
             eLocalVarsBytesCounter = funLocalVarsBytesCounter,
             eVarTypes = funLocalTypes
           }
-  undefined
+  code <- generateCode (A.SBStmt noPos body)
+  return $ funEpilogue <> code <> funPrologue
 
 compileClass :: A.UIdent -> StmtTEval String
 compileClass = undefined
